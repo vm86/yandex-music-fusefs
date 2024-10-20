6d80d92c973510904c1101ec0340f5f8b56543e2
Roman Nebaluev <rnebaluev@gmail.com>
2024-10-21 02:48:18 +0500

    fix install systemd service

--- setup.py
+++ setup.py
@@ -8,9 +8,13 @@ SERVICE_NAME = "yamusic-fs.service"
 
 
 def install() -> None:
+    install_system_service_path = Path.home().joinpath(
+        f".config/systemd/user/{SERVICE_NAME}"
+    )
+    install_system_service_path.parent.mkdir(parents=True, exist_ok=True)
     shutil.copy2(
         Path(f"contrib/{SERVICE_NAME}"),
-        Path.home().joinpath(f".config/systemd/user/{SERVICE_NAME}"),
+        install_system_service_path,
     )
     check_call(["systemctl", "daemon-reload", "--user"])
     check_call(["systemctl", "enable", SERVICE_NAME, "--user"])
